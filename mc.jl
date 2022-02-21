fc87fd081cf43a9fce08a1e13764b54b3aeaff8a
mikhail-a-ivanov <mikhail.ivanov.1995@gmail.com>
2022-02-21 18:59:14 +0100

    added XYZ trajectory and energy output

--- mc.jl
+++ mc.jl
@@ -1,5 +1,7 @@
 using Printf
 using RandomNumbers
+using StaticArrays
+
 
 """
 pbcdx(x1, x2, xsize)
@@ -8,7 +10,7 @@ Compute periodic boundary distance between x1 and x2
 """
 function pbcdx(x1, x2, xsize)
     dx = x2 - x1
-    dx += -xsize * convert(Int, round(dx/xsize))
+    dx += -xsize * convert(Int32, round(dx/xsize))
     return(dx)
 end
 
@@ -34,12 +36,12 @@ separated by scaled Rm distance
 and the periodic box vectors in reduced units
 """
 function ljlattice(latticePoints, latticeScaling)
-    lattice = [convert(Vector{Float32}, [i, j, k]) 
+    lattice = [convert(SVector{3, Float32}, [i, j, k]) 
         for i in 0:latticePoints-1 for j in 0:latticePoints-1 for k in 0:latticePoints-1]
     lattice = lattice .* (2^(1/6) * latticeScaling)
     # Generate PBC box vectors
     boxSide = (latticePoints) * (2^(1/6) * latticeScaling)
-    box = [boxSide, boxSide, boxSide]
+    box = convert(SVector{3, Float32}, [boxSide, boxSide, boxSide])
     return(lattice, box)
 end
 
@@ -82,27 +84,6 @@ function particleenergy(conf, box, pointIndex)
 end
 
 """
-writexyz(conf, outname="conf.xyz", atomtype="Ar")
-
-Writes and XYZ file of a system snapshot
-for visualization in VMD
-"""
-function writexyz(conf, σ, outname="conf.xyz", atomtype="Ar")
-    io = open(outname, "w")
-    print(io, length(conf), "\n\n")
-    for i in 1:length(conf)
-        print(io, atomtype, " ")
-        for j in 1:3
-            print(io, @sprintf("%10.3f", conf[i][j]*σ), " ")
-            if j == 3
-                print(io, "\n")
-            end
-        end
-    end
-    close(io)
-end
-
-"""
 mcmove(E, Tred, conf, box, delta)
 
 Performs a Metropolis Monte Carlo
@@ -114,7 +95,7 @@ function mcmove(conf, box, E, Tred, delta, rng)
     E1 = particleenergy(conf, box, pointIndex)
 
     # Displace a particle and compute the new energy
-    dr = [delta*(rand(rng, Float32) - 0.5), delta*(rand(rng, Float32) - 0.5), delta*(rand(rng, Float32) - 0.5)]
+    dr = SVector{3, Float32}(delta*(rand(rng, Float32) - 0.5), delta*(rand(rng, Float32) - 0.5), delta*(rand(rng, Float32) - 0.5))
     conf[pointIndex] += dr
     
     E2 = particleenergy(conf, box, pointIndex)
@@ -151,24 +132,64 @@ function mcrun(steps, outfreq, conf, box, Tred, delta, rng)
     E = totalenergy(conf, box)
     energies[1] = E
 
-    confs = [[zeros(3) for i in 1:length(conf)] for j in 1:floor(Int, steps/outfreq)]
+    confs = [[SVector{3, Float32}(zeros(3)) for i in 1:length(conf)] for j in 1:(floor(Int, steps/outfreq) + 1)]
     confs[1] = conf
 
     # Acceptance counter
     acceptedTotal = 0
 
     # Run MC simulation
-    for i in 1:steps
+    @inbounds @fastmath for i in 1:steps
         newconf, newE, accepted = mcmove(conf, box, energies[i], Tred, delta, rng)
         energies[i + 1] = newE
         acceptedTotal += accepted
         if i % outfreq == 0
-            confs[Int(i/outfreq)] = newconf
+            confs[Int(i/outfreq) + 1] = newconf
         end
     end
     acceptanceRatio = acceptedTotal / steps
     return(confs, energies[1:outfreq:end], acceptanceRatio)
 end
+
+"""
+writexyz(conf, outname="conf.xyz", atomtype="Ar")
+
+Writes and XYZ file of a system snapshot
+for visualization in VMD
+"""
+function writexyz(conf, currentStep, σ, append=false, outname="conf.xyz", atomtype="Ar")
+    if append
+        io = open(outname, "a")
+    else
+        io = open(outname, "w")
+    end
+    print(io, length(conf), "\n")
+    print(io, "Step = ", @sprintf("%d", currentStep), "\n")
+    for i in 1:length(conf)
+        print(io, atomtype, " ")
+        for j in 1:3
+            print(io, @sprintf("%10.3f", conf[i][j]*σ), " ")
+            if j == 3
+                print(io, "\n")
+            end
+        end
+    end
+    close(io)
+end
+
+function writeenergies(energy, currentStep, append=false, outname="energies.dat")
+    if append
+        io = open(outname, "a")
+    else
+        io = open(outname, "w")
+    end
+    print(io, "# Total energy in reduced units \n")
+    print(io, "# Step = ", @sprintf("%d", currentStep), "\n")
+    print(io, @sprintf("%10.3f", energy), "\n")
+    print(io, "\n")
+    close(io)
+end
+
 """
 Main function for running MC simulation
 """
@@ -176,14 +197,22 @@ function main()
     @time begin
     println("Running MC simulation...")
     # Initialize parameters
+    # Constants
     kB = 1.38064852E-23 # [J/K]
-    σ = 3.345 # [Å]
-    ϵ = 125.7*kB # [J]; original value in ϵ/kB [K]
-    T = 95 # [K]
-    Tred = T*kB/ϵ # In reduced units
+    amu = 1.66605304E-27 # [kg]
+    # Forcefield
+    atommass = 39.792 # [amu]
+    σ = 3.405 # [Å]
+    ϵ = 119.8*kB # [J]; original value in ϵ/kB [K]
+    # Conditions
+    T = 95 # target temperature [K]
+    density = 1374 # target density [kg/m3]
     delta = 0.2 # Max displacement [σ]
-    lattice_points = 10
-    lattice_scaling = 1
+    lattice_points = 10 # Number of lattice points
+    # Other parameters
+    density_Rm = (amu*atommass / (2^(1/6) * σ * 1E-10)^3) # Initial density
+    Tred = T*kB/ϵ # Temperature in reduced units
+    lattice_scaling = (density_Rm / density)^(1/3) # Scaling to target density
 
     # Generate LJ lattice
     conf, box = ljlattice(lattice_points, lattice_scaling)
@@ -193,20 +222,29 @@ function main()
     E = totalenergy(conf, box)
     @printf("Starting energy = %.3f epsilon\n", E)
 
-    # Save configuration to XYZ
-    writexyz(conf, σ, "start.xyz")
+    # Save initial configuration to XYZ
+    writexyz(conf, 0, σ, false, "start.xyz")
 
     # Run MC simulation
-    steps = Int(1E6) # MC steps
-    outfreq = Int(1E5) # Output frequency
+    steps = Int(1E5) # MC steps
+    outfreq = Int(1E4) # Output frequency
 
-    rng_xor = RandomNumbers.Xorshifts.Xoroshiro128Star()
+    rng_xor = RandomNumbers.Xorshifts.Xoroshiro128Plus()
     confs, energies, acceptanceRatio = mcrun(steps, outfreq, conf, box, Tred, delta, rng_xor)
     println("Acceptance ratio = ", acceptanceRatio)
     println("Energies = ", round.(energies, digits=3), "\n")
 
+    # Save MC trajectory and energies
+    writexyz(confs[1], 0, σ, false, "mc-traj.xyz")
+    writeenergies(energies[1], 0, false, "energies.dat")
+    
+    for i in 2:length(energies)
+        writeenergies(energies[i], (i-1)*outfreq, true, "energies.dat")
+        writexyz(confs[i], (i-1)*outfreq, σ, true, "mc-traj.xyz")
+    end
+
     # Save final configuration to XYZ
-    writexyz(confs[end], σ, "end.xyz")
+    writexyz(confs[end], steps, σ, false, "end.xyz")
     end
 end
 
